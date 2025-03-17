d387f9e4382532abeb7f6868231d043eb271b1cd
Aizon <killian.bonneau@iia-formation.fr>
2025-03-17 10:06:14 +0100

    Base entities definition

---
+++ src/Entity/CreditCard.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CreditCardRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: CreditCardRepository::class)]
+class CreditCard
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 19)]
+    private ?string $number = null;
+
+    #[ORM\Column(length: 3)]
+    private ?string $cvv = null;
+
+    #[ORM\Column]
+    #[Assert\Range(min: 1, max: 12)]
+    private ?int $expirationMonth = null;
+
+    #[ORM\Column]
+    #[Assert\Range(min: 2000, max: 2100)]
+    private ?int $expirationYear = null;
+
+    /**
+     * @var Collection<int, Payment>
+     */
+    #[ORM\OneToMany(targetEntity: Payment::class, mappedBy: 'creditCard')]
+    private Collection $payments;
+
+    #[ORM\Column(length: 500)]
+    private ?string $holderName = null;
+
+    public function __construct()
+    {
+        $this->payments = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNumber(): ?string
+    {
+        return $this->number;
+    }
+
+    public function setNumber(string $number): static
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    public function getFilteredNumberBeginEnd(): ?string
+    {
+        return
+            substr($this->number, 0, 4) .
+            ' **** **** ' .
+            substr($this->number, 8, 4);
+    }
+
+    public function getFilteredNumberEnd(): ?string
+    {
+        return '**** **** **** ' . substr($this->number, 8, 4);
+    }
+
+    public function getCvv(): ?string
+    {
+        return $this->cvv;
+    }
+
+    public function setCvv(string $cvv): static
+    {
+        $this->cvv = $cvv;
+
+        return $this;
+    }
+
+    public function getExpirationMonth(): ?int
+    {
+        return $this->expirationMonth;
+    }
+
+    public function setExpirationMonth(int $expirationMonth): static
+    {
+        $this->expirationMonth = $expirationMonth;
+
+        return $this;
+    }
+
+    public function getExpirationYear(): ?int
+    {
+        return $this->expirationYear;
+    }
+
+    public function setExpirationYear(int $expirationYear): static
+    {
+        $this->expirationYear = $expirationYear;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Payment>
+     */
+    public function getPayments(): Collection
+    {
+        return $this->payments;
+    }
+
+    public function addPayment(Payment $payment): static
+    {
+        if (!$this->payments->contains($payment)) {
+            $this->payments->add($payment);
+            $payment->setCreditCard($this);
+        }
+
+        return $this;
+    }
+
+    public function removePayment(Payment $payment): static
+    {
+        if ($this->payments->removeElement($payment)) {
+            // set the owning side to null (unless already changed)
+            if ($payment->getCreditCard() === $this) {
+                $payment->setCreditCard(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getHolderName(): ?string
+    {
+        return $this->holderName;
+    }
+
+    public function setHolderName(string $holderName): static
+    {
+        $this->holderName = $holderName;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Payment.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PaymentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PaymentRepository::class)]
+class Payment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $amount = null;
+
+    #[ORM\Column]
+    private ?bool $isRefunded = null;
+
+    #[ORM\ManyToOne(inversedBy: 'payments')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Product $product = null;
+
+    #[ORM\ManyToOne(inversedBy: 'payments')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    #[ORM\ManyToOne(inversedBy: 'payments')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?CreditCard $creditCard = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAmount(): ?string
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(string $amount): static
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function isRefunded(): ?bool
+    {
+        return $this->isRefunded;
+    }
+
+    public function setIsRefunded(bool $isRefunded): static
+    {
+        $this->isRefunded = $isRefunded;
+
+        return $this;
+    }
+
+    public function getProduct(): ?Product
+    {
+        return $this->product;
+    }
+
+    public function setProduct(?Product $product): static
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getCreditCard(): ?CreditCard
+    {
+        return $this->creditCard;
+    }
+
+    public function setCreditCard(?CreditCard $creditCard): static
+    {
+        $this->creditCard = $creditCard;
+
+        return $this;
+    }
+}
